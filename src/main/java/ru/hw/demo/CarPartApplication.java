5c5a6967e69ceba82f9924b56d7b2541df4c0609
AKaporov <voropak@yandex.ru>
2022-12-30 12:18:44 +0300

    убрал лишнее из main

--- src/main/java/ru/hw/demo/CarPartApplication.java
+++ src/main/java/ru/hw/demo/CarPartApplication.java
@@ -2,16 +2,12 @@ package ru.hw.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CarPartApplication {
 
-    public static void main(String[] args) /*throws SQLException */ {
-        ConfigurableApplicationContext ctx = SpringApplication.run(CarPartApplication.class, args);
-
-//		System.out.println("Hello world");
-//		Console.main(args);
+    public static void main(String[] args) {
+        SpringApplication.run(CarPartApplication.class, args);
     }
 
 }
