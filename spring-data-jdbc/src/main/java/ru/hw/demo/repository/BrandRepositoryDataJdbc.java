8554d216898732ff721401404b651bb162ca5901
AKaporov <voropak@yandex.ru>
2023-07-07 14:08:54 +0300

    fix: Data-JDBC переименование репозиториев

--- spring-data-jdbc/src/main/java/ru/hw/demo/repository/BrandRepositoryDataJdbc.java
+++ spring-data-jdbc/src/main/java/ru/hw/demo/repository/BrandRepositoryDataJdbc.java
@@ -1,12 +1,13 @@
 package ru.hw.demo.repository;
 
 import org.springframework.data.domain.Example;
+import org.springframework.data.repository.Repository;
 import ru.hw.demo.domain.Brand;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface BrandRepositoryDataJdbc /*extends Repository<Brand, Long>*/ {
+public interface BrandRepositoryDataJdbc extends Repository<Brand, Long> {
     List<Brand> findAll(Example<Brand> example);
 
     void deleteAll();
--- spring-data-jdbc/src/main/java/ru/hw/demo/repository/CarPartRepositoryDataJdbc.java
+++ spring-data-jdbc/src/main/java/ru/hw/demo/repository/CarPartRepositoryDataJdbc.java
@@ -1,10 +1,12 @@
 package ru.hw.demo.repository;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
 import ru.hw.demo.domain.CarPart;
 
 import java.util.Optional;
 
-public interface CarPartRepositoryDataJdbc /*extends  CrudRepository<CarPart, Long>, JpaSpecificationExecutor<CarPart> */ {
+public interface CarPartRepositoryDataJdbc extends CrudRepository<CarPart, Long>, JpaSpecificationExecutor<CarPart> {
 
     /**
      * @param vendorCode каталожный номер
