463a34ff8abee3bebb2476f333af2b207c7906f1
AKaporov <voropak@yandex.ru>
2023-09-18 18:55:40 +0300

    fix:CommandLineRunner. application+configuration

--- command-line-runner/src/main/java/ru/hw/demo/config/CommandLineRunnerConfigurator.java
+++ command-line-runner/src/main/java/ru/hw/demo/config/CommandLineRunnerConfigurator.java
@@ -1,55 +1,52 @@
 package ru.hw.demo.config;
 
 import org.junit.jupiter.api.Order;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.hw.demo.constant.MainConstant;
 import ru.hw.demo.domain.*;
-import ru.hw.demo.repository.*;
+import ru.hw.demo.repository.BrandRepository;
+import ru.hw.demo.repository.CarPartRepository;
+import ru.hw.demo.repository.EngineRepository;
+import ru.hw.demo.repository.ModelRepository;
 
 import java.util.List;
 
 @Configuration
 public class CommandLineRunnerConfigurator {
 
-    public static final String BRAND_GAZ = "Gaz";
-    public static final String BRAND_URAL = "Ural";
-    public static final String BRAND_KAMAZ = "Kamaz";
-//    @Autowired
-//    private static BrandRepository brandRepository;
+    @Bean
+    @Order(1)
+    public CommandLineRunner brandsCommandLineRunner(BrandRepository brandRepository) {
+        return args -> {
+            brandRepository.save(Brand.builder().name(MainConstant.BRAND_KAMAZ).build());
+            brandRepository.save(Brand.builder().name(MainConstant.BRAND_URAL).build());
+//            brandRepository.save(Brand.builder().name(MainConstant.BRAND_GAZ).build());
 
-//    @Bean
-//    @Order(1)
-//    public CommandLineRunner brandsCommandLineRunner(BrandRepository brandRepository) {
-//        return args -> {
-//            brandRepository.save(Brand.builder().name(BRAND_KAMAZ).build());
-//            brandRepository.save(Brand.builder().name(BRAND_URAL).build());
-//            brandRepository.save(Brand.builder().name(BRAND_GAZ).build());
-//
-//        };
-//    }
-//
-//    @Bean
-//    @Order(2)
-//    public CommandLineRunner modelsCommandLineRunner(ModelRepository modelRepository) {
-//        return args -> {
+        };
+    }
+
+    @Bean
+    @Order(2)
+    public CommandLineRunner modelsCommandLineRunner(ModelRepository modelRepository) {
+        return args -> {
 //            modelRepository.save(Model.builder().name("Gaz-66").yearRelease(1964).build());
-//            modelRepository.save(Model.builder().name("Ural-4320").yearRelease(1977).build());
-//            modelRepository.save(Model.builder().name("Kamaz 65201").yearRelease(2000).build());
-//        };
-//    }
-//
-//    @Bean
-//    @Order(3)
-//    public CommandLineRunner enginesCommandLineRunner(EngineRepository repository) {
-//        return args -> {
+            modelRepository.save(Model.builder().name("Ural-4320").yearRelease(1977).build());
+            modelRepository.save(Model.builder().name("Kamaz 65201").yearRelease(2000).build());
+        };
+    }
+
+    @Bean
+    @Order(3)
+    public CommandLineRunner enginesCommandLineRunner(EngineRepository repository) {
+        return args -> {
 //            repository.save(Engine.builder().name("Diesel").build());
-//            repository.save(Engine.builder().name("Petrol").build());
-//            repository.save(Engine.builder().name("Turbocharged diesel").build());
-//        };
-//    }
-//
+            repository.save(Engine.builder().name("Petrol").build());
+            repository.save(Engine.builder().name("Turbocharged diesel").build());
+        };
+    }
+
 //    @Bean
 //    @Order(4)
 //    public CommandLineRunner countriesCommandLineRunner(CountryRepository repository) {
@@ -57,35 +54,74 @@ public class CommandLineRunnerConfigurator {
 //            repository.save(Country.builder().name("Russia").build());
 //        };
 //    }
-//
-//    @Bean
-//    @Order(5)
-//    public CommandLineRunner carPartsCommandLineRunner(CarPartRepository repository) {
-//        return args -> {
-//            carPartLoader(repository);
-//        };
-//    }
-//
-//    private static void carPartLoader(CarPartRepository carPartRepository) {
+
+    @Bean
+    @Order(5)
+    public CommandLineRunner carPartsCommandLineRunner(CarPartRepository carPartRepository, BrandRepository brandRepository) {
+        return args -> {
+            carPartLoader(carPartRepository, brandRepository);
+        };
+    }
+
+    private static void carPartLoader(CarPartRepository carPartRepository, BrandRepository brandRepository) {
 //        List<Brand> brands = brandRepository.findAll();
 //
 //        Brand brandGaz = brands.stream()
-//                .filter(brand -> BRAND_GAZ.equalsIgnoreCase(brand.getName()))
+//                .filter(brand -> MainConstant.BRAND_GAZ.equals(brand.getName()))
 //                .findFirst()
 //                .get();
-//
-//        CarPart save = carPartRepository.save(CarPart.builder()
-//                .vendorCode("GZ-750Z370-S")
-//                .sku("SKU-201902-0057")
-//                .name("ДИСК СЦЕПЛЕНИЯ")
-//                .description("Диск сцепления передает крутящий момент от ДВС к трансмиссии, выступает в качестве составляющей трения.")
-//                .price(59720.66d)
-//                .manufacturer("ZF SACHS")
-//                .rating(9.7d)
+
+        CarPart carPartOne = CarPart.builder()
 //                .brand(brandGaz)
-//                .build());
-//
-//
-//        System.out.println("****** KAPOROV = " + save);
-//    }
+                .brand(Brand.builder().name(MainConstant.BRAND_GAZ).build())
+                .model(Model.builder().name("Gaz-66").yearRelease(1964).build())
+                .engine(Engine.builder().name("Diesel").build())
+                .country(Country.builder().name("Russia").build())
+                .vendorCode("GZ-750Z370-S")
+                .sku("SKU-201902-0057")
+                .name("ДИСК СЦЕПЛЕНИЯ")
+                .description("Диск сцепления передает крутящий момент от ДВС к трансмиссии, выступает в качестве составляющей трения.")
+                .price(59720.66d)
+                .manufacturer("ZF SACHS")
+                .rating(9.7d)
+                .photoList(
+                        List.of(
+                                Photo.builder().photoUrl("https://localhost:8080/carpart/1/#&gid=1&pid=1").build(),
+                                Photo.builder().photoUrl("https://localhost:8080/carpart/1/#&gid=1&pid=2").build(),
+                                Photo.builder().photoUrl("https://localhost:8080/carpart/1/#&gid=1&pid=3").build())
+                )
+                .build();
+
+        CarPart build = CarPart.builder()
+//                .brand(brandGaz)
+                .brand(Brand.builder().name(MainConstant.BRAND_GAZ).build())
+                .model(Model.builder().name("Gaz-66").yearRelease(1964).build())
+                .engine(Engine.builder().name("Diesel").build())
+                .country(Country.builder().name("Russia").build())
+                .vendorCode("GZ-750Z370-S")
+                .sku("SKU-201902-0057")
+                .name("ДИСК СЦЕПЛЕНИЯ")
+                .description("Диск сцепления передает крутящий момент от ДВС к трансмиссии, выступает в качестве составляющей трения.")
+                .price(59720.66d)
+                .manufacturer("ZF SACHS")
+                .rating(9.7d)
+                .photoList(
+                        List.of(
+                                Photo.builder().photoUrl("https://localhost:8080/carpart/1/#&gid=1&pid=1").build(),
+                                Photo.builder().photoUrl("https://localhost:8080/carpart/1/#&gid=1&pid=2").build(),
+                                Photo.builder().photoUrl("https://localhost:8080/carpart/1/#&gid=1&pid=3").build())
+                )
+                .build();
+
+        CarPart savedCarPartOne = carPartRepository.save(carPartOne);
+//        CarPart savedCarPartTwo = carPartRepository.save(build);
+//        System.out.println("****** KAPOROV = " + savedCarPartTwo);
+
+        Analog analogCarPartOne = Analog.builder().vendor("No name (China)").carPart(savedCarPartOne).build();
+        savedCarPartOne.setAnalogList(List.of(analogCarPartOne));
+
+        CarPart save1 = carPartRepository.save(savedCarPartOne);
+        System.out.println("****** KAPOROV = " + savedCarPartOne);
+        System.out.println("****** KAPOROV = " + save1);
+    }
 }
