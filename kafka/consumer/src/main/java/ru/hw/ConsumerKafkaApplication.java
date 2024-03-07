fbdb14e6ef274c9421b812e64a70a4ca658755e5
AKaporov <voropak@yandex.ru>
2024-03-07 18:57:51 +0300

    feat: Kafka создание модулей producer, consumer

--- kafka/consumer/src/main/java/ru/hw/ConsumerKafkaApplication.java
+++ kafka/consumer/src/main/java/ru/hw/ConsumerKafkaApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaApplication {
-
+public class ConsumerKafkaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(KafkaApplication.class, args);
+        SpringApplication.run(ConsumerKafkaApplication.class, args);
     }
 }
